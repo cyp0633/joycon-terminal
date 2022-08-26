884cf875302a98f2d8dc28b9bf2522fc52794749
cyp0633 <cyp0633@gmail.com>
2022-08-26 15:36:57 +0800

    Add responsive alert window & input box

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,21 +1,24 @@
 <script setup>
 import { reactive } from 'vue'
 import { Greet, ConnectSerial } from '../../wailsjs/go/main/App'
-import {NButton,NInput} from 'naive-ui'
+import { NButton, NInput, NAlert, NSpace,NCode } from 'naive-ui'
 
 const data = reactive({
 	name: "",
 	resultText: "等待连接",
 	status: false,
+	alertBox: "info",
 })
 
 function connect() {
 	if (data.name == "") {
 		data.resultText = "请输入串口名称或路径"
+		data.alertBox = "error"
 		return
 	}
 	if (data.status == true) {
 		data.resultText = "串口已连接"
+		data.alertBox = "warning"
 		return
 	}
 	ConnectSerial(data.name).then(result => {
@@ -23,6 +26,10 @@ function connect() {
 	})
 	if (result == "success") {
 		data.status = true
+		data.alertBox = "success"
+	}
+	else {
+		data.alertBox = "error"
 	}
 }
 
@@ -33,13 +40,12 @@ function connect() {
 		<div id="tip" class="tip h-16 mx-auto">
 			<h3>输入串口名/路径</h3>对于 Windows，尝试 <code class="code">COMx</code>；否则，尝试 <code class="code">/dev/ttyUSBx</code>
 		</div>
-		<div id="input" class="input-box">
-			<!-- <input id="name" v-model="data.name" autocomplete="off" class="input" type="text" /> -->
-			<n-input id="name" v-model="data.name" autocomplete="off" class="m-1.5 w-5" type="text" placeholder="串口名" />
-			<!-- <button class="btn" @click="connect">连接</button> -->
+		<n-space align="center" justify="center">
+			<n-input id="name" v-model:value="data.name" class="m-1.5 w-2/6" type="text" placeholder="串口名" />
 			<n-button type="primary" @click="connect" class="m-1.5">连接</n-button>
-		</div>
-		<div id="result" class="result">{{ data.resultText }}</div>
+		</n-space>
+		<n-alert title="连接状态" v-bind:type="data.alertBox" class="w-2/6 mx-auto inset-x-0">{{ data.resultText }}
+		</n-alert>
 	</main>
 </template>
 
@@ -49,12 +55,6 @@ function connect() {
 	line-height: 20px;
 	margin: 1.5rem auto;
 }
-
-/* .tip {
-	height: 50px;
-	line-height: 20px;
-	margin: 1.5rem auto;
-} */
 
 .input-box .btn {
 	width: 60px;
